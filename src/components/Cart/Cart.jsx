d948ac968310eb68ce2d076991b16e7660cf1b8d
sharin45593 <maria45593sharin@gmail.com>
2023-03-28 11:36:11 +0600

    cart calculation

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+    console.log(cart)
+
+    let total = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        total =total + product.price;
+        totalShipping =total +product.shipping;
+    }
+    const tax =total*7/100;
+    const grandTotal = total +totalShipping +tax;
+    return (
+        <div className='cart'>
+            <h3>Order Summary</h3>
+                <p>Selected Items: {cart.length}</p> 
+                <p>Total Price :$ {total}</p>
+                <p>Total Shipping :$ {totalShipping} </p>
+                <p>Tax: $ {tax .toFixed(2)}</p>
+                <h4>Grand Total:$ {grandTotal.toFixed(2) } </h4>
+        </div>
+    );
+};
+
+export default Cart;
