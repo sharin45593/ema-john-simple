c67b09a1a6673b180fb39456e659ea8e0de857e5
sharin45593 <maria45593sharin@gmail.com>
2023-03-28 15:26:51 +0600

    exsisting cart set

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,19 @@ const Cart = ({cart}) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total =total + product.price;
+        product.quantity = product.quantity || 1;
+        total =total + product.price *product.quantity;
         totalShipping =total +product.shipping;
+        quantity =quantity + product.quantity;
     }
     const tax =total*7/100;
     const grandTotal = total +totalShipping +tax;
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-                <p>Selected Items: {cart.length}</p> 
+                <p>Selected Items: {quantity}</p> 
                 <p>Total Price :$ {total}</p>
                 <p>Total Shipping :$ {totalShipping} </p>
                 <p>Tax: $ {tax .toFixed(2)}</p>
